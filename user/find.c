1aeb3053f86b51fcafd0447a6a554f1a9f696c64
thanhtran <147932040+tranthanh22@users.noreply.github.com>
2025-03-03 10:10:05 +0700

    update find.c

--- user/find.c
+++ user/find.c
@@ -3,53 +3,52 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 
-void find(char *path, char *target){
+void find(char *path, char *target) {
     char buf[512], *p;
     int fd;
     struct stat st;
     struct dirent de;
 
-    if ((fd = open(path, 0)) < 0){
+    if ((fd = open(path, 0)) < 0) {
         printf("find: cannot open %s\n", path);
         return;
     }
 
-    if (fstat(fd, &st) < 0){
+    if (fstat(fd, &st) < 0) {
         printf("find: cannot stat %s\n", path);
         close(fd);
         return;
     }
 
-    if (st.type == T_FILE){
-        if (strcmp(path + strlen(path) - strlen(target), target) == 0){
+    if (st.type == T_FILE) {
+        if (strcmp(path + strlen(path) - strlen(target), target) == 0) {
             printf("%s\n", path);
         }
         close(fd);
         return;
     }
 
-    if (st.type == T_DIR){
-        if (strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf)){
+    if (st.type == T_DIR) {
+        if (strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf)) {
             printf("find: path too long\n");
             close(fd);
             return;
         }
 
-        strcpy(buf, path);
-        p = buf + strlen(buf);
-        *p++ = '/';
-
-        while (read(fd, &de, sizeof(de)) == sizeof(de)){
+        while (read(fd, &de, sizeof(de)) == sizeof(de)) {
             if (de.inum == 0)
                 continue;
 
             if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
                 continue;
 
+            strcpy(buf, path);
+            p = buf + strlen(buf);
+            *p++ = '/';
             memmove(p, de.name, DIRSIZ);
             p[DIRSIZ] = 0;
 
-            if (stat(buf, &st) < 0){
+            if (stat(buf, &st) < 0) {
                 printf("find: cannot stat %s\n", buf);
                 continue;
             }
@@ -63,10 +62,9 @@ void find(char *path, char *target){
     }
     close(fd);
 }
-
-int
-main(int argc, char *argv[]){
-    if (argc != 3){
+    
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
         printf("Usage: find <directory> <filename>\n");
         exit(1);
     }
