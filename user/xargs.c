e454e1a48cee515135b14bb76b80a5b328e18e84
thanhtran <147932040+tranthanh22@users.noreply.github.com>
2025-03-03 08:39:37 +0700

    update xargs

--- user/xargs.c
+++ user/xargs.c
@@ -5,31 +5,50 @@
 
 #define MAXLEN 512  
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     char buf[MAXLEN];  
     char *cmd[MAXARG]; 
     int i, n = 0;
-
-    for (i = 1; i < argc && i < MAXARG - 1; i++)
-        cmd[i - 1] = argv[i];
-
-    while ((n = read(0, buf, sizeof(buf))) > 0){
+    int base_args = 1;
+    int single_arg_mode = 0;
+    if (argc > 2 && strcmp(argv[1], "-n") == 0 && strcmp(argv[2], "1") == 0) {
+        single_arg_mode = 1;
+        base_args = 3; 
+    }
+    for (i = base_args; i < argc && i < MAXARG - 1; i++) {
+        cmd[i - base_args] = argv[i];
+    }
+    int fixed_args = argc - base_args;
+    int arg_count = fixed_args;
+    while ((n = read(0, buf, sizeof(buf))) > 0) {
         int j = 0;
-        for (i = 0; i < n; i++){
-            if (buf[i] == '\n'){  
-                buf[i] = 0;  
-                cmd[argc - 1] = buf + j;  
-                cmd[argc] = 0;  
-
-                if (fork() == 0){
-                    exec(cmd[0], cmd);
-                    exit(1);
-                }
-                else
+        for (i = 0; i < n; i++) {
+            if (buf[i] == '\n') {  
+                buf[i] = 0; 
+                if (single_arg_mode) {
+                    cmd[fixed_args] = buf + j;
+                    cmd[fixed_args + 1] = 0;
+                    if (fork() == 0) {
+                        exec(cmd[0], cmd);
+                        fprintf(2, "exec failed\n");
+                        exit(1);
+                    }
                     wait(0);
-                j = i + 1;
+                } else {
+                    cmd[arg_count++] = buf + j;
+                }
+                j = i + 1; 
             }
         }
+    }
+    if (!single_arg_mode) {
+        cmd[arg_count] = 0;
+        if (fork() == 0) {
+            exec(cmd[0], cmd);
+            fprintf(2, "exec failed\n");
+            exit(1);
+        }
+        wait(0);
     }
     exit(0);
 }
